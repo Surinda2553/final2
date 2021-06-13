69433cb6517620dd3ca82a1757b3e5449d3d5316
Surinda Sarakum <surindasarakum18@gmail.com>
2021-06-13 16:13:03 +0700

    Add project files.

---
+++ final2/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace final2
+{
+    class Program
+    {
+        /* static void sort(int[] arr)
+         {
+             int n = arr.Length;
+
+             for (int i = 0; i < n - 1; i++)
+             {
+                 int min_idx = i;
+                 for (int j = i + 1; j < n; j++)
+                     if (arr[j] < arr[min_idx])
+                         min_idx = j;
+                 int temp = arr[min_idx];
+                 arr[min_idx] = arr[i];
+                 arr[i] = temp;
+             }
+         }
+         static void printArray(int[] arr)
+         {
+             int n = arr.Length;
+             for (int i = 0; i < n; ++i)
+                 Console.Write(arr[i] + " ");
+             Console.WriteLine();
+         }
+         public static void Main()
+         {
+             int[] arr = { 64, 25, 12, 22, 11 };
+             sort(arr);
+             Console.WriteLine("Sorted array");
+             printArray(arr);
+         }*/
+
+        /*static void bubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+
+        public static void Main()
+        {
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            bubbleSort(arr);
+            Console.WriteLine("Sorted array");
+            printArray(arr);
+
+
+        }*/
+
+        /*static void bubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+
+        public static void Main()
+        {
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            bubbleSort(arr);
+            Console.WriteLine("Sorted array");
+            printArray(arr);
+
+
+        }*/
+
+
+        class InsertionSort
+        {
+            void sort(int[] arr)
+            {
+                int n = arr.Length;
+                for (int i = 1; i < n; ++i)
+                {
+                    int key = arr[i];
+                    int j = i - 1;
+                    while (j >= 0 && arr[j] > key)
+                    {
+                        arr[j + 1] = arr[j];
+                        j = j - 1;
+                    }
+                    arr[j + 1] = key;
+                }
+            }
+            static void printArray(int[] arr)
+            {
+                int n = arr.Length;
+                for (int i = 0; i < n; ++i)
+                    Console.Write(arr[i] + " ");
+
+                Console.Write("\n");
+            }
+            public static void Main()
+            {
+                int[] arr = { 12, 11, 13, 5, 6 };
+                InsertionSort ob = new InsertionSort();
+                ob.sort(arr);
+                printArray(arr);
+            }
+        }
+    }
+}
